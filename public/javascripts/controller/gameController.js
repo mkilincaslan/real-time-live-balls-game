c5abe2dd700e35cc22711d6469eee26b6a0875c0
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2019-05-09 12:17:59 +0300

    services prepared

--- public/javascripts/controller/gameController.js
+++ public/javascripts/controller/gameController.js
@@ -1,3 +1,13 @@
-app.controller('gameController', ['$scope', ($scope) => {
+app.controller('gameController', ['$scope', 'indexFactory',  ($scope, indexFactory) => {
 
+    indexFactory.connectSocket('http://localhost:3000', {
+        reconnectionAttempts: 3,
+        reconnectionDelay: 600
+    })
+        .then(socket => {
+            console.log('Successfull connection - ', socket);
+        })
+        .catch(error => {
+           console.log(error);
+        });
 }]);
